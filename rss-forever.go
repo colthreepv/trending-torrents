39f29161d27c3f2cc2f9a56427b9368d2c63afde
mrgamer <mister.gamer@gmail.com>
2014-05-11 02:01:42 +0200

    implementation of RSS, it's bad design. you need to download RSS, that is a custom XML, then convert to JSON, and is not even what i want.

--- rss-forever.go
+++ rss-forever.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
-	"github.com/ungerik/go-rss"
-	// "net/http"
+	"code.google.com/p/go.net/html"
+	"net/http"
 	"os"
 	"time"
 )
@@ -30,16 +32,56 @@ func (lst *lastFetch) calc() time.Duration {
 	return lst.lastRun
 }
 
-func fetchRSS() (err error) {
-	// _, err = http.Get("http://kickass.to/new/rss/")
-	rssChan, err := rss.Read("http://kickass.to/new/rss/")
-	if err != nil {
+type Torrent struct {
+	OrientClass string `json:"@class"`
+	Category []string `json:"category"`
+	Description string `json:"description"`
+	Hash string `json:"hash"`
+	Link string `json:"link"`
+	PubDate time.Time `json:"pubDate"`
+	Title string `json:"title"`
+}
+
+func NewTorrentFromRss (i rss.Item) *Torrent, error {
+	ti := new(Torrent)
+	ti.Title = i.Title
+	ti.Category = 
+}
+
+func fetchRSS() error {
+	list, listErr := http.Get("http://kickass.to/new/")
+	// rssChan, err := rss.Read("http://kickass.to/new/")
+	if listErr != nil {
 		return err
 	}
 	for _, item := range rssChan.Item {
+
 		fmt.Printf("item read: %s\n", item.Title)
+		/* prepare RSS-XML to JSON
+		jsonifiedItem, jErr := json.Marshal(item)
+		if jErr != nil {
+			return jErr
+		}
+
+		// build request
+		req, hErr := http.NewRequest("POST", "http://localhost:2480/document/trending/", bytes.NewReader(jsonifiedItem))
+		if hErr != nil {
+			return hErr
+		}
+		req.SetBasicAuth("root", "brolazzo0")
+		req.Header.Add("Content-Type", "application/json;charset=UTF-8")
+
+		fmt.Printf("debug request: %s\n", jsonifiedItem)
+		// send it
+		resp, rErr := http.DefaultClient.Do(req)
+		if rErr != nil {
+			return rErr
+		}
+		fmt.Printf("response from DB: %v\n", resp.StatusCode)
+		*/
+		return nil // debug, stop only after first one
 	}
-	return err
+	return nil
 }
 
 func main() {
