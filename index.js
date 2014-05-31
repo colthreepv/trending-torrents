d8f6cad91a80e0620193dd3e8c90607f87eee09d
mrgamer <mister.gamer@gmail.com>
2014-05-31 18:17:43 +0200

    split the code in different files, modularized

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ var cluster = require('cluster'),
     fs = require('fs');
 
 // libs
-var gzipRequest = require('./lib/gzip-request.js');
+var gzipRequest = require('./lib/gzip-request.js'),
+    kat = require('./lib/kat');
 
 // external deps
 var cheerio = require('cheerio'),
@@ -17,50 +18,20 @@ cluster.setupMaster({
 });
 var workersArr = [];
 for (var i = 0; i < numCpu; i++) {
-  workersArr.push(cluster.fork());
+  cluster.fork();
 }
 
 // listen in case of child death, debugging reasons only.. for now
-workersArr.forEach(function (worker) {
+Array.prototype.forEach.call(cluster.workers, function (worker) {
   worker.on('exit', function (code, signal) {
     console.log('a child exited with code:', code, 'and signal', signal);
   });
 });
 
-function KatFetchCollection (howmany) {
-  var board = [];
-  for (var i = 0; i < howmany; i++) {
-    board[i] = false;
-  }
-
-  this.activeFetchers = 0;
-  this.current = 0;
-  this.data = [];
-  this.failures = {};
-  this.board = board;
-}
-
-KatFetchCollection.prototype.getPage = function () {
-  var freePage;
-  if (freePage = this.board.indexOf(false), freePage !== -1) {
-    this.board[freePage] = true;
-    this.activeFetchers++;
-    console.log('active fetchers:', this.activeFetchers);
-    return freePage + 1;
-  }
-  return null;
-};
-
-KatFetchCollection.prototype.success = function (data) {
-  this.data.push(data);
-  this.activeFetchers--;
-};
-
-// KatScout.
-gzipRequest({
-  url:'http://kickass.to/new/',
-  timeout: 60 * 1000
-}, function (err, body) {
+// KatScout with retry
+async.retry(function (callback, results) {
+  gzipRequest('http://kickass.to/new/', callback);
+}, function scoutDone (err, body) {
   if (err) {
     return console.log('KatScout failed:', err);
   }
@@ -74,7 +45,7 @@ gzipRequest({
 
   // processing
   var workers = numCpu,
-      fetchCollection = new KatFetchCollection(20);
+      fetchCollection = new kat.KatFetchCollection(20);
 
   // done, we start workers now.
   async.whilst(function testWhilst() { // OLOLOL, i don't have to use whilst, but .queue :(
@@ -87,12 +58,12 @@ gzipRequest({
     }
 
     // dispatch work to a worker
-    workersArr[workers - 1].send({
+    cluster.workers[workers - 1].send({
       url: 'http://kickass.to/new/' + pageToFetch + '/'
     });
 
     // and listen once for completion
-    workersArr[workers - 1].once('message', function (msg) {
+    cluster.workers[workers - 1].once('message', function (msg) {
       if (msg.data) {
         fetchCollection.success(msg.data);
         return callback();
--- worker.js
+++ worker.js
@@ -1,95 +1,8 @@
+
 console.log('i\'m an happy worker, :)');
 
 // libs
-var gzipRequest = require('./lib/gzip-request.js');
-
-// external deps
-var cheerio = require('cheerio');
-
-function parseSize (size) {
-  var splitSize = size.split(' ');
-  var numericSize = parseFloat(splitSize[0]);
-
-  switch (splitSize[1]) {
-    case 'bytes':
-      return numericSize;
-    case 'KB':
-      return numericSize * 1024;
-    case 'MB':
-      return numericSize * 1024 * 1024;
-    case 'GB':
-      return numericSize * 1024 * 1024 * 1024;
-    default:
-      console.log('size not handled:', splitSize[1]);
-      return numericSize;
-  }
-}
-
-function parseAge (timeAgo) {
-  var splitTime = timeAgo.split(/\s/); // WARNING: ' ' didn't work as splitter, &nbsp in there!
-  var numericTime = parseInt(splitTime[0], 10);
-  var timeNow = Date.now();
-
-  switch (splitTime[1]) {
-    case 'sec.':
-      timeNow = timeNow - (numericTime * 1000);
-      break;
-    case 'min.':
-      timeNow = timeNow - (numericTime * 60 * 1000);
-      break;
-    case 'hour':
-    case 'hours':
-      timeNow = timeNow - (numericTime * 60 * 60 * 1000);
-      break;
-    case 'day':
-      timeNow = timeNow - (numericTime * 24 * 60 * 60 * 1000);
-      break;
-    case 'week':
-      timeNow = timeNow - (numericTime * 7 * 24 * 60 * 60 * 1000);
-      break;
-    default:
-      console.log('timeAgo not handled:', splitTime[1]);
-      return new Date();
-  }
-
-  return new Date(timeNow);
-}
-
-function KatFetch() {
-  this.startTime = Date.now();
-  this.elapsed  = null;
-  this.data = [];
-}
-
-// callback must comply sendDataBack (err, data)
-KatFetch.prototype.fetch = function (url, callback) {
-  var self = this;
-  gzipRequest(url, function (err, body) {
-    if (err) {
-      return callback(err);
-    }
-
-    var jq = cheerio.load(body);
-    var torrentElements = jq('table .odd, table .even');
-    torrentElements.each(function (index, torrent) {
-      torrent = cheerio(torrent);
-
-      var rowData = {
-        name: torrent.find('.cellMainLink').text(),
-        magnet: torrent.find('.imagnet').attr('href'),
-        size: parseSize(torrent.find('.nobr.center').text()),
-        files: parseInt(torrent.find('.nobr.center').next().text(), 10),
-        age: parseAge(torrent.find('.nobr.center').next().next().text())
-      };
-      console.log('parsing torrent:', index);
-      self.data[index] = rowData;
-    });
-
-    // write elapsed time
-    self.elapsed = Date.now() - self.startTime;
-    callback(null, self);
-  });
-};
+var kat = require('./lib/kat');
 
 function sendDataBack(err, data) {
   if (err) {
@@ -105,7 +18,7 @@ function sendDataBack(err, data) {
 
 process.on('message', function (msg) {
   if (msg.url) {
-    var singleFetch = new KatFetch();
+    var singleFetch = new kat.KatFetch();
     singleFetch.fetch(msg.url, sendDataBack);
   }
   console.log('dumping the message:', msg);
